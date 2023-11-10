0450e50c0ca6cc05d99a8b57bb18892c90e12a36
Pavel Sanicovich <rapidcodelab.mx@gmail.com>
2023-11-10 17:52:38 +0300

    format code

--- src/provider.js
+++ src/provider.js
@@ -12,28 +12,25 @@ let reqBody = {}
 reqBody.entities = entity_ids;
 
 (async (nodes) => {
-
-let payloads = await fetch(endpoint, {
-  method: 'POST',
-  headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify(reqBody)
+  
+  let payloads = await fetch(endpoint, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(reqBody)
+    })
+    .then(res => res.json())
+    .then(res => {
+      return res.payloads
+    })
+    .catch(e => console.log(e));
+  
+  
+  payloads.forEach(payload => {
+    const result = Array.from(nodes)
+      .find((node) => node.dataset.entityId === payload.entity_id);
+    result.innerHTML += payload.adm
   })
-  .then(res => res.json())
-  .then(res => {
-    return res.payloads
-  })
-  .catch(e => console.log(e));
-
-  console.log(nodes)
-
-payloads.forEach(payload => {
- const result = Array.from(nodes).find((node) => node.dataset.entityId === payload.entity_id);
- console.log(result)
- result.innerHTML += payload.adm
-})
- })(entities)
-
-
+})(entities)
